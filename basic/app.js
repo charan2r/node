934e02c432047273a08e8e1714da2c83d5f53e78
Charan <charanromi325@gmail.com>
2024-07-15 22:04:00 +0530

    new

--- basic/app.js
+++ basic/app.js
@@ -1,2 +1,12 @@
-console.log("hello")
+const {sum} = require("./helper");
+const http = require("http");
+
+const server = http.createServer((req,res) => {
+    res.end("Hello world");
+});
+server.listen(3000);
+
+
+const total = sum(10,20);
+console.log(total);
 
